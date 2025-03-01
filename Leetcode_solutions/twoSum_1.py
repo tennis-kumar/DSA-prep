caa799a1a5e2e0cecf90d418197632a860610b02
tennis <kumarteneesh@gmail.com>
2025-03-01 23:16:00 +0530

    Refactor twoSum solution to use hashmap for optimal performance,
    note: some test cases may fail in leetcode for the two pointer solution as index value will change since we are sorting the array

--- Leetcode_solutions/twoSum_1.py
+++ Leetcode_solutions/twoSum_1.py
@@ -6,10 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # approach 1 brute force
-        # Time complexity is O(n^2) and space complexity is O(1)
-        # This method is not efficient.
-        
+        # approach 1 brute force method
         # for i in range(len(nums)-1):
         #     for j in range(i+1,len(nums)):
         #         if nums[i]+nums[j] == target:
@@ -17,21 +14,32 @@ class Solution(object):
 
         # return
 
-
-
-         # approach 2 using two pointer approach
-        nums.sort() # soring the array 
-        left, right = 0, len(nums)-1
-
-        while left < right:
-            sum = nums[left]+nums[right]
-
-            if sum == target:
-                return (left,right)
-            elif sum < target:
-                left +=1
-            elif sum >target:
-                right -=1
-        return
-    
         
+        # approach 2 using two pointer approach
+        # nums.sort() # soring the array 
+        # left, right = 0, len(nums)-1
+
+        # while left < right:
+        #     sum = nums[left]+nums[right]
+
+        #     if sum == target:
+        #         return (left,right)
+        #     elif sum < target:
+        #         left +=1
+        #     elif sum >target:
+        #         right -=1
+        # return
+
+
+        # approach 3 using hashmap/dictonary
+
+        index_map = {}
+
+        for i,num in enumerate(nums):
+            complement = target - num
+
+            if complement in index_map:
+                return [index_map[complement],i]
+            index_map[num] = i
+        
+        return
