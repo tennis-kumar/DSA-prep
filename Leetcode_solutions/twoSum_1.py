7701e5d5f9385f6d28349d0254c4c01fd8009628
tennis <kumarteneesh@gmail.com>
2025-03-01 23:08:28 +0530

    Refactor twoSum solution to implement two-pointer approach for improved efficiency

--- Leetcode_solutions/twoSum_1.py
+++ Leetcode_solutions/twoSum_1.py
@@ -10,11 +10,28 @@ class Solution(object):
         # Time complexity is O(n^2) and space complexity is O(1)
         # This method is not efficient.
         
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return (i,j)
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return (i,j)
 
+        # return
+
+
+
+         # approach 2 using two pointer approach
+        nums.sort() # soring the array 
+        left, right = 0, len(nums)-1
+
+        while left < right:
+            sum = nums[left]+nums[right]
+
+            if sum == target:
+                return (left,right)
+            elif sum < target:
+                left +=1
+            elif sum >target:
+                right -=1
         return
     
         
