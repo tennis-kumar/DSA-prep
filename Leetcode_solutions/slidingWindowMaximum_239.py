967e4a828b86bc4b5a507f82dab9a0a44c862852
tennis <kumarteneesh@gmail.com>
2025-03-09 23:08:07 +0530

    day 7 progress, done with sliding window problems , will review tomorrow on overall progress and decide on next steps, how we proceed further in this journey

---
+++ Leetcode_solutions/slidingWindowMaximum_239.py
@@ -0,0 +1,21 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums: return []
+
+        dq = deque()
+        result = []
+
+        for i in range(len(nums)):
+            if dq and dq[0] < i - k + 1:
+                dq.popleft()
+
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            
+            dq.append(i)
+
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+        
+        return result
